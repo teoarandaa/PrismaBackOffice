a662f03be4d3ab91f3f76ba3b2581f10514b62d5
Teo Aranda Páez <145863288+teoarandaa@users.noreply.github.com>
2025-02-17 18:00:22 +0100

    routes for CRUD for client added

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/clientes', [ClienteController::class, 'store']);
+
+// Si quieres crear un grupo de rutas para clientes, puedes hacerlo así:
+Route::prefix('clientes')->group(function () {
+    Route::post('/', [ClienteController::class, 'store']);
+    Route::get('/', [ClienteController::class, 'index']);
+    Route::get('/{cliente}', [ClienteController::class, 'show']);
+    Route::put('/{cliente}', [ClienteController::class, 'update']);
+    Route::delete('/{cliente}', [ClienteController::class, 'destroy']);
+});
